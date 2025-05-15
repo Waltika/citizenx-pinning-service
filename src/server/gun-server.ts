c36c18b55650943c869ebcee0f3d14fc88a36713
Waltika <walter.wartenweiler@gmail.com>
2025-05-15 21:59:24 +0200

    Tweaking og:url and canonical

--- src/server/gun-server.ts
+++ src/server/gun-server.ts
@@ -658,13 +658,13 @@ app.get('/viewannotation/:annotationId/:base64Url', async (req: Request, res: Re
     <meta property="og:title" content="${title}">
     <meta property="og:description" content="${description}">
     <meta property="og:image" content="${image}">
-    <meta property="og:url" content="${canonicalUrl}">
+    <meta property="og:url" content="${publicUrl}">
     <meta property="og:type" content="website">
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="${title}">
     <meta name="twitter:description" content="${description}">
     <meta name="twitter:image" content="${image}">
-    <link rel="canonical" href="${canonicalUrl}">
+    <link rel="canonical" href="${publicUrl}">
 </head>
 <body>
     <script>
