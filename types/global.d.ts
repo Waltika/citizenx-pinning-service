c4f6bb4e56515694add9db92ed1cdca1c19b2d96
Waltika <walter.wartenweiler@gmail.com>
2025-05-15 20:48:52 +0200

    Clearing old crap.

--- types/global.d.ts
+++
@@ -1,4 +0,0 @@
-// types/global.d.ts
-interface Window {
-    __INITIAL_DATA__?: { url: string; annotationId: string };
-}
--- types/sea.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'gun/sea.js' {
-    interface SEA {
-        verify(data: string, publicKey: string): Promise<boolean>;
-        // Add other SEA methods as needed
-    }
-    const SEA: SEA;
-    export default SEA;
-}
